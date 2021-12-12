c485f563ca4471db544a485dbc84fdab4d7be5b5
surendra <surendra.kovela@gmail.com>
2021-12-12 15:22:11 +0530

    first

---
+++ Details.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("Name:K.V.Surendra");
+    printf("  Branch:CSE");
+    printf("   MY HOBBIES ARE READING BOOKS AND PLAYING CRIKET AND BADMINTON");
+    return 0;
+}
